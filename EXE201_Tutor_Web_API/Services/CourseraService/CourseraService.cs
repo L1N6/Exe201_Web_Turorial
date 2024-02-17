8e8eed4f94e7ae4566a837007e2ee6732ba1065f
DoQuocThanh <thanhdq2302@gmail.com>
2024-02-17 09:19:13 +0700

    push

--- EXE201_Tutor_Web_API/Services/CourseraService/CourseraService.cs
+++ EXE201_Tutor_Web_API/Services/CourseraService/CourseraService.cs
@@ -4,6 +4,7 @@ using EXE201_Tutor_Web_API.Base.Service;
 using EXE201_Tutor_Web_API.Dto;
 using EXE201_Tutor_Web_API.Entites;
 using EXE201_Tutor_Web_API.Repositories.CourseraRepositoryPlace;
+using Microsoft.EntityFrameworkCore;
 
 namespace EXE201_Tutor_Web_API.Services.CourseraService
 {
@@ -17,6 +18,11 @@ namespace EXE201_Tutor_Web_API.Services.CourseraService
             _mapper = mapper;
         }
 
+        public IQueryable<CourseraDto> GetAllByFilter()
+        {
+            var entities = _courseraRepository.GetAll().Include(x=>x.CourseraId);
+            return _mapper.Map<IQueryable<CourseraDto>>(entities);
+        }
 
     }
 }
