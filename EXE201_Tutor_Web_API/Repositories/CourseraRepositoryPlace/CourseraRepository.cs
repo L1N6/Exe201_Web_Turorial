35b6fc17fcd7d3a9ed009512393bc076dd79e6c3
DoQuocThanh <thanhdq2302@gmail.com>
2024-02-19 22:01:12 +0700

    up

--- EXE201_Tutor_Web_API/Repositories/CourseraRepositoryPlace/CourseraRepository.cs
+++ EXE201_Tutor_Web_API/Repositories/CourseraRepositoryPlace/CourseraRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace EXE201_Tutor_Web_API.Repositories.CourseraRepositoryPlace
-{
-    public class CourseraRepository
-    {
+﻿using EXE201_Tutor_Web_API.Base.Repository;
+using EXE201_Tutor_Web_API.Database;
+using EXE201_Tutor_Web_API.Entites;
 
+namespace EXE201_Tutor_Web_API.Repositories.CourseraRepositoryPlace
+{
+    public class CourseraRepository : Repository<Coursera, int>
+    {
+        private readonly Exe201_Tutor_Context _context;
+        public CourseraRepository(Exe201_Tutor_Context context) : base(context)
+        {
+            _context = context;
+        }
     }
 }
