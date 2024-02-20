7252903dd16e32afade502e7b6b37a7318dea592
DoQuocThanh <thanhdq2302@gmail.com>
2024-02-20 22:53:08 +0700

    update

--- EXE201_Tutor_Web/Program.cs
+++ EXE201_Tutor_Web/Program.cs
@@ -1,51 +1,21 @@
-using EXE201_Tutor_Web.Models;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
 
-namespace EXE201_Tutor_Web
+
+using EXE201_Tutor_Web;
+
+namespace Extension
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var services = builder.Services;
-            var configuration = builder.Configuration;
-            //services.AddAuthentication(options =>
-            //{
-            //    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            //    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-            //}).AddCookie().AddGoogle(googleOptions =>
-            //{
-            //    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
-            //    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
-            //});
-
-            services.AddHttpClient();
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddSession();
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            app.UseSession();
-
-            app.UseRouting();
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
