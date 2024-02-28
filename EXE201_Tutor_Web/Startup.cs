b13d3f155fa475dc08590b1c17483ec71bbc7fa3
L1N6 <119463897+L1N6@users.noreply.github.com>
2024-02-28 09:48:03 +0700

    update

--- EXE201_Tutor_Web/Startup.cs
+++ EXE201_Tutor_Web/Startup.cs
@@ -77,7 +77,7 @@ namespace EXE201_Tutor_Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Admin}/{action=Student}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
